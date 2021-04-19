d5406419470f52a8f768ce48051e25765a6d3236
Aidan McGuire <>
2021-04-19 10:20:26 -0600

    Add chef method ingredients

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,5 +1,8 @@
-class Chef <ApplicationRecord
+class Chef < ApplicationRecord
   validates_presence_of :name
   has_many :dishes
 
+  def ingredients
+    self.dishes.joins(:ingredients).distinct.order(calories: :desc).pluck("ingredients.name, calories").to_h
+  end
 end
--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -7,4 +7,22 @@ RSpec.describe Chef, type: :model do
   describe "relationships" do
     it {should have_many :dishes}
   end
+
+  describe 'instance methods' do
+    describe 'ingredients' do
+      it 'returns all ingredients used by chef, no duplicates, by calories desc' do
+        chef = Chef.create!(name: "Anthony")
+        dish1 = chef.dishes.create!(name: "Yum", description: "Very yum")
+        dish2 = chef.dishes.create!(name: "Tasty", description: "Very tasty")
+        ingredient2 = dish1.ingredients.create!(name: "carrot", calories: 20)
+        ingredient6 = dish2.ingredients.create!(name: "onion", calories: 10)
+        ingredient1 = dish1.ingredients.create!(name: "onion", calories: 10)
+        ingredient5 = dish2.ingredients.create!(name: "butter", calories: 50)
+        ingredient4 = dish2.ingredients.create!(name: "garlic", calories: 40)
+        ingredient3 = dish1.ingredients.create!(name: "potato", calories: 30)
+
+        expect(chef.ingredients).to eq({"butter"=>50, "garlic"=>40, "potato"=>30, "carrot"=>20, "onion"=>10})
+      end
+    end
+  end
 end
