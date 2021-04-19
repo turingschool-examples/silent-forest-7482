0cb0b36a5df4de3ec62a5487dd40cd33bc8495f5
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-04-19 11:47:48 -0500

    Add migrations, complete user stories 1 and 2

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,4 +2,8 @@ class Chef <ApplicationRecord
   validates_presence_of :name
   has_many :dishes
 
+  def unique_ingredients_ordered_by_calories
+    dishes.joins(:ingredients).select('ingredients.*').order('ingredients.calories DESC').distinct
+  end
+
 end
---
+++ spec/features/chefs/show_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe 'chef show page', type: :feature do
+  before :each do
+    @chef = Chef.create!(name: 'Gordan Ramsay')
+    @dish_1 = @chef.dishes.create!(name: 'Spaghetti', description: 'delicious pasta')
+    @dish_2 = @chef.dishes.create!(name: 'Margarita Pizza', description: 'I love pizza')
+    @dish_3 = @chef.dishes.create!(name: 'Fish Taco', description: 'Taco Tuesday')
+    @ingredient_1 = Ingredient.create!(name: 'tomato sauce', calories: 50)
+    @ingredient_2 = Ingredient.create!(name: 'pasta', calories: 300)
+    @ingredient_3 = Ingredient.create!(name: 'cheese', calories: 200)
+    @ingredient_4 = Ingredient.create!(name: 'fish', calories: 350)
+    DishIngredient.create!(dish: @dish_1, ingredient: @ingredient_1)
+    DishIngredient.create!(dish: @dish_1, ingredient: @ingredient_2)
+    DishIngredient.create!(dish: @dish_2, ingredient: @ingredient_1)
+    DishIngredient.create!(dish: @dish_2, ingredient: @ingredient_3)
+    DishIngredient.create!(dish: @dish_2, ingredient: @ingredient_3)
+    DishIngredient.create!(dish: @dish_3, ingredient: @ingredient_3)
+    DishIngredient.create!(dish: @dish_3, ingredient: @ingredient_4)
+    visit chef_path(@chef.id)
+  end
+
+  it 'can see the name of the chef' do
+    expect(page).to have_content(@chef.name)
+  end
+
+  it 'can see the nams and descriptions of all dishes' do
+    expect(page).to have_content(@dish_1.name)
+    expect(page).to have_content(@dish_1.description)
+
+    expect(page).to have_content(@dish_2.name)
+    expect(page).to have_content(@dish_2.description)
+
+    expect(page).to have_content(@dish_3.name)
+    expect(page).to have_content(@dish_3.description)
+  end
+
+  it 'can see all unique ingredients for all dishes the chef makes ordered from most to least calories' do
+    expect(page).to have_content(@ingredient_1.name)
+    expect(page).to have_content(@ingredient_1.calories)
+    expect(page).to have_content(@ingredient_2.name)
+    expect(page).to have_content(@ingredient_2.calories)
+    expect(page).to have_content(@ingredient_3.name)
+    expect(page).to have_content(@ingredient_3.calories)
+    expect(page).to have_content(@ingredient_4.name)
+    expect(page).to have_content(@ingredient_4.calories)
+
+    expect(@ingredient_3.name).to appear_before(@ingredient_1.name)
+    expect(@ingredient_4.name).to appear_before(@ingredient_1.name)
+    expect(@ingredient_4.name).to appear_before(@ingredient_3.name)
+  end
+end
