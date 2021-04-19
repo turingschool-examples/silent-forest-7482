478174838efd066e28d1418d7424d6ab36f0f6f2
Brisa Garcia <brisagarciaglz@gmail.com>
2021-04-19 12:52:04 -0600

    Add dependant destroy to get test to work

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -4,17 +4,12 @@ class DishesController < ApplicationController
   # end
 
   def destroy
-    @chef = Chef.find_by(name: params[:name])
-    @dish = Dish.find(params[:id]).destroy
-    Recipe.where("id = ?", params[:chef_id] )
-    # dish = Dish.find(params[:id])
-    # dish.destroy
-    #
-    # chef = Chef.find(params[:chef_id])
-    # @chef = Chef.find(params[:chef_id])
-    # @dish = Dish.find(params[:id])
-    #
-    # @dish.destroy
+    # binding.pry
+   chef = Chef.find(params[:chef_id])
+   dish = Dish.find(params[:id])
+   dish.destroy
+
+
 
     redirect_to "/chefs/#{chef.id}"
   end
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,7 +1,7 @@
 class Dish <ApplicationRecord
   validates_presence_of :name, :description
   belongs_to :chef
-  has_many :recipes
+  has_many :recipes, dependent: :destroy
   has_many :ingredients, through: :recipes
 
 end
