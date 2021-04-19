d153267fddd5021fc24a072ae1210e91c7f61eb1
Klaudia Stewart <klaudiastewart@gmail.com>
2021-04-19 09:27:12 -0600

    Create all tables and model specs for all tables

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Ingredient, type: :model do
   describe "validations" do
-    it {should validate_presence_of :name}
-    it {should validate_presence_of :calories}
+    it {should validate_presence_of(:name)}
+    it {should validate_presence_of(:calories)}
   end
   describe "relationships" do
     it {should have_many(:dishes).through(:dish_ingredients)}
