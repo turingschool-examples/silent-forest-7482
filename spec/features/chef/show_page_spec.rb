8b3ac4cc7fd5cad7d011243780e67425680d290f
Klaudia Stewart <klaudiastewart@gmail.com>
2021-04-19 10:49:13 -0600

    Add specs to chef show feature page and chef model to complete us2

--- spec/features/chef/show_page_spec.rb
+++ spec/features/chef/show_page_spec.rb
@@ -5,6 +5,11 @@ RSpec.describe "Chef Show Page" do
     @chef = Chef.create!(name: "Kernel Sanders")
     @chicken = @chef.dishes.create!(name: "Kentucky Fried Chicken", description: "Very crispy chicken")
     @mashed_potatoes = @chef.dishes.create!(name: "Mashed Potatoes", description: "Very buttery potatoes")
+    @batter = @chicken.ingredients.create!(name: "Chicken Batter", calories: 450)
+    @butter = @mashed_potatoes.ingredients.create!(name: "Butter", calories: 500)
+    DishIngredient.create!(dish: @chicken, ingredient: @batter)
+    DishIngredient.create!(dish: @mashed_potatoes, ingredient: @butter)
+    DishIngredient.create!(dish: @mashed_potatoes, ingredient: @batter)
     visit "/chefs/#{@chef.id}"
   end
 
@@ -15,6 +20,22 @@ RSpec.describe "Chef Show Page" do
     expect(page).to have_content(@chicken.description)
     expect(page).to have_content(@mashed_potatoes.name)
     expect(page).to have_content(@mashed_potatoes.description)
-    save_and_open_page
+  end
+
+  it 'shows the name and calories of all ingredients this chef uses in their dishes' do
+    expect(page).to have_content(@batter.name)
+    expect(page).to have_content(@batter.calories)
+    expect(page).to have_content(@butter.name)
+    expect(page).to have_content(@butter.calories)
+  end
+
+  it 'shows unique ingredient names' do
+    expect(page).to have_content(@batter.name, count: 1)
+    expect(page).to have_content(@butter.name, count: 1)
+  end
+
+  it 'shows ingredients ordered from most calories to least' do
+    expect(page.all(".ingredient")[0].text).to eq("#{@butter.name}, #{@butter.calories}")
+    expect(page.all(".ingredient")[1].text).to eq("#{@batter.name}, #{@batter.calories}")
   end
 end
--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -7,4 +7,23 @@ RSpec.describe Chef, type: :model do
   describe "relationships" do
     it {should have_many :dishes}
   end
+
+  describe 'instance methods' do
+    before(:each) do
+      @chef = Chef.create!(name: "Kernel Sanders")
+      @chicken = @chef.dishes.create!(name: "Kentucky Fried Chicken", description: "Very crispy chicken")
+      @mashed_potatoes = @chef.dishes.create!(name: "Mashed Potatoes", description: "Very buttery potatoes")
+      @batter = @chicken.ingredients.create!(name: "Chicken Batter", calories: 450)
+      @butter = @mashed_potatoes.ingredients.create!(name: "Butter", calories: 500)
+      DishIngredient.create!(dish: @chicken, ingredient: @batter)
+      DishIngredient.create!(dish: @mashed_potatoes, ingredient: @butter)
+      DishIngredient.create!(dish: @mashed_potatoes, ingredient: @batter)
+    end
+    describe '#ingredient_list' do
+      it 'returns unique ingredients ordered from highest calories to lowest' do
+        expect(@chef.ingredient_list.first.name).to eq(@butter.name)
+        expect(@chef.ingredient_list.last.name).to eq(@batter.name)
+      end
+    end
+  end
 end
