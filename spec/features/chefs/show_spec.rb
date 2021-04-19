6a709e60840bf6f058774c40aef41a3c621fb604
koshea9 <keegan.oshea9@gmail.com>
2021-04-19 10:16:33 -0600

    Add feature and model tests for US 2

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -3,9 +3,16 @@ require 'rails_helper'
 RSpec.describe 'the chefs show page' do
   before(:each) do
     @chef_1 = Chef.create!(name: "Amy")
+    @dish_1 = @chef_1.dishes.create!(name: "Alfredo", description: "Creamy Pasta")
+    @dish_2 = @chef_1.dishes.create!(name: "Carrot Pie", description: "Pup fav")
+    @ing_1 = Ingredient.create!(name: "carrots", calories: 100)
+    @ing_2 = Ingredient.create!(name: "milk", calories: 200)
+    @ing_3 = Ingredient.create!(name: "cheese", calories: 300)
+    Recipe.create!(dish: @dish_1, ingredient: @ing_1)
+    Recipe.create!(dish: @dish_1, ingredient: @ing_2)
+    Recipe.create!(dish: @dish_2, ingredient: @ing_3)
+
     @chef_2 = Chef.create!(name: "Bob")
-    @alfredo = @chef_1.dishes.create!(name: "Alfredo", description: "Creamy Pasta")
-    @carrots = @chef_1.dishes.create!(name: "Carrots", description: "Pup fav")
     @bob_dish = @chef_2.dishes.create!(name: "Bob's dish", description: "Bobs fav")
   end
 
@@ -13,10 +20,21 @@ RSpec.describe 'the chefs show page' do
     visit "/chefs/#{@chef_1.id}"
 
     expect(page).to have_content(@chef_1.name)
-    expect(page).to have_content(@alfredo.name)
-    expect(page).to have_content(@alfredo.description)
+    expect(page).to have_content(@dish_1.name)
+    expect(page).to have_content(@dish_1.description)
     expect(page).to_not have_content(@chef_2.name)
     expect(page).to_not have_content(@bob_dish.description)
-    save_and_open_page
+  end
+
+  it 'displays a list of ingredients used in dishes from most calories to least' do
+    visit "/chefs/#{@chef_1.id}"
+
+    expect(page).to have_content(@chef_1.ingredient_calories_list)
+    expect(page).to have_content(@ing_1.name)
+    expect(page).to have_content(@ing_1.calories)
+    expect(page).to have_content(@ing_2.name)
+    expect(page).to have_content(@ing_2.calories)
+    expect(page).to have_content(@ing_3.name)
+    expect(page).to have_content(@ing_3.calories)
   end
 end
--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -7,4 +7,25 @@ RSpec.describe Chef, type: :model do
   describe "relationships" do
     it {should have_many :dishes}
   end
+
+  describe 'instance methods' do
+    before(:each) do
+      @chef_1 = Chef.create!(name: "Amy")
+      @dish_1 = @chef_1.dishes.create!(name: "Alfredo", description: "Creamy Pasta")
+      @dish_2 = @chef_1.dishes.create!(name: "Carrot Pie", description: "Pup fav")
+      @ing_1 = Ingredient.create!(name: "carrots", calories: 100)
+      @ing_3 = Ingredient.create!(name: "cheese", calories: 300)
+      @ing_2 = Ingredient.create!(name: "milk", calories: 200)
+      Recipe.create!(dish: @dish_1, ingredient: @ing_1)
+      Recipe.create!(dish: @dish_1, ingredient: @ing_2)
+      Recipe.create!(dish: @dish_2, ingredient: @ing_3)
+    end
+
+    describe "#ingredient_calories_list" do
+      it 'returns a unique list of ingredients ordered from most to least calories' do
+
+      expect(@chef_1.ingredient_calories_list).to eq([@ing_3,@ing_2,@ing_1])
+      end
+    end
+  end
 end
