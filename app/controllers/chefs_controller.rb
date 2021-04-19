f98a776435973c05f111ada3e8c54b984e826125
austinandrade <austinmandrade@gmail.com>
2021-04-19 13:06:21 -0400

    Include only show and hard params per user stories

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -3,17 +3,6 @@ class ChefsController < ApplicationController
     @chef = Chef.find(chef_params[:id])
   end
 
-  def update
-    # pet = Pet.find(params[:id])
-    # if pet.update(pet_params)
-    #   redirect_to show page route
-    # else
-      #string interpolate here; can't because of snippet
-      # redirect_to "/pets/pet.id/edit",
-      #alert: "Error: string interpolate hashtag error_message(merchant.errors)
-    # end
-  end
-
   private
 
   def chef_params
