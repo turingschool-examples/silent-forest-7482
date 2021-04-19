12eab6c9ffa300866d673d2a2009889750b83ff8
r-stemmle <ryan.stemmle@gmail.com>
2021-04-19 11:29:22 -0400

    Add chefs dish names and descriptions to show page

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -5,17 +5,26 @@ RSpec.describe "Chef Show Page" do
     context "When I visit a chefs show page" do
       before :each do
         @chef = Chef.create!(name: "Mario")
-
+        @dish_1 = Dish.create!(name: 'lasagna', description: 'vegan with red sauce', chef_id: @chef.id)
+        @dish_2 = Dish.create!(name: 'pizza', description: 'cheese and pepperoni', chef_id: @chef.id)
+        @dish_3 = Dish.create!(name: 'steak', description: 'filet with pepper sauce', chef_id: @chef.id)
         visit chef_path(@chef)
       end
 
       it "Then I see the name of the chef" do
-
         expect(page).to have_content(@chef.name)
       end
 
       it "And I see the names and descriptions of all the chefs dishes" do
-        
+        within ".dish-describe" do
+          expect(page).to have_content(@dish_1.name)
+          expect(page).to have_content(@dish_2.name)
+          expect(page).to have_content(@dish_3.name)
+          expect(page).to have_content(@dish_1.description)
+          expect(page).to have_content(@dish_2.description)
+          expect(page).to have_content(@dish_3.description)
+          save_and_open_page
+        end
       end
     end
   end
