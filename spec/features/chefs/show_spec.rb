91f00faa137f5ba11431721067ace25f42265471
George Ho <george.h.w.ho@gmail.com>
2021-04-19 13:00:19 -0400

    Add feature test functionality to view

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -70,7 +70,8 @@ describe 'chef show page' do
 
       @dish_1 = Dish.create!(name: "dish_1", description: 'this is dish 1', chef_id: @chef.id)
       @dish_2 = Dish.create!(name: "dish_2", description: 'this is dish 2', chef_id: @chef.id)
-      @dish_2 = Dish.create!(name: "dish_3", description: 'this is dish 3', chef_id: @chef.id)
+      @dish_3 = Dish.create!(name: "dish_3", description: 'this is dish 3', chef_id: @chef.id)
+      @dish_4 = Dish.create!(name: "dish_4", description: 'this is dish 4', chef_id: @chef.id)
 
       @ingredient_1 = Ingredient.create!(name: "ingredient_1", calories: 10)
       @ingredient_2 = Ingredient.create!(name: "ingredient_2", calories: 20)
@@ -96,18 +97,19 @@ describe 'chef show page' do
     }
 
     it 'shows only the top 3 dishes' do
+      expect(page).to have_content("Most Popular Ingredients")
       within '.most-popular-ingredients' do
-        expect(page).to have_content(@dish_1.name)
-        expect(page).to have_content(@dish_2.name)
-        expect(page).to have_content(@dish_3.name)
-        expect(page).to_not have_content(@dish_4.name)
+        expect(page).to have_content(@ingredient_1.name)
+        expect(page).to have_content(@ingredient_2.name)
+        expect(page).to have_content(@ingredient_3.name)
+        expect(page).to_not have_content(@ingredient_4.name)
       end
     end
 
     it 'shows the dishes ordered by popularity of usage' do
       within '.most-popular-ingredients' do
-        expect(@dish_1.name).to appear_before(@dish_2.name)
-        expect(@dish_2.name).to appear_before(@dish_3.name)
+        expect(@ingredient_1.name).to appear_before(@ingredient_2.name)
+        expect(@ingredient_2.name).to appear_before(@ingredient_3.name)
       end
     end
   end
