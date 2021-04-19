8273a92d7446182d3b412dee2ccaf2f2e8182582
Joe Ray <jray100.jr@gmail.com>
2021-04-19 11:41:07 -0600

    Add method for chef ingredients

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,4 +1,11 @@
 class Chef <ApplicationRecord
   validates_presence_of :name
   has_many :dishes
+
+  def ingredients_ordered_by_calories
+    Ingredient.joins(:dish_ingredients, :ingredients)
+    .group(:id)
+    .order(calories: :desc)
+    .limit(1)
+  end
 end
