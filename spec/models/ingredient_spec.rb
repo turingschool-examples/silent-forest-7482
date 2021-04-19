7375d10aa66cd077ed8f476310d4d01b713aca63
George Ho <george.h.w.ho@gmail.com>
2021-04-19 11:23:10 -0400

    Add validations and tests for ingredients and dish_ingredients

---
+++ spec/models/ingredient_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Ingredient, type: :model do
+  describe "validations" do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :calories}
+  end
+  describe "relationships" do
+    it {should have_many :dishes}
+  end
+end
