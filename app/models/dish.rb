0f66d05fcc66632ece20460697b6105bd4477ae0
austinandrade <austinmandrade@gmail.com>
2021-04-19 11:16:50 -0400

    Create models, validations, and associations

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,4 +1,6 @@
 class Dish <ApplicationRecord
   validates_presence_of :name, :description
   belongs_to :chef
+  has_many :dishes_ingredients
+  has_many :ingredients, through: :dishes_ingredients
 end
---
+++ app/models/ingredient.rb
@@ -0,0 +1,5 @@
+class Ingredient <ApplicationRecord
+  validates_presence_of :name, :calories
+  has_many :dishes_ingredients
+  has_many :dishes, through: :dishes_ingredients
+end
