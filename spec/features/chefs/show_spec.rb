b3811db77d589db95f3d74fce8da0673df74cc66
George Ho <george.h.w.ho@gmail.com>
2021-04-19 11:39:39 -0400

    Add feature test for user story 2

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -9,6 +9,7 @@ describe 'chef show page' do
 
       visit chef_path(@chef)
     }
+
     it 'shows you the name of the chef' do
       expect(page).to have_content("George")
     end
@@ -18,6 +19,39 @@ describe 'chef show page' do
       expect(page).to have_content("this is dish 1")
       expect(page).to have_content("dish_2")
       expect(page).to have_content("this is dish 2")
+    end
+  end
+
+  context 'you look at the ingredients section' do
+    before {
+      @chef = Chef.create!(name: "George")
+
+      @dish_1 = Dish.create!(name: "dish_1", description: 'this is dish 1', chef_id: @chef.id)
+      @dish_2 = Dish.create!(name: "dish_2", description: 'this is dish 2', chef_id: @chef.id)
+
+      @ingredient_1 = Ingredient.create!(name: "ingredient_1", calories: 10)
+      @ingredient_2 = Ingredient.create!(name: "ingredient_2", calories: 20)
+
+      @dish_1.ingredients << @ingredient_1
+      @dish_1.ingredients << @ingredient_2
+      @dish_2.ingredients << @ingredient_1
+
+      visit chef_path(@chef)
+    }
+
+    it 'shows the name and calories for all ingredients' do
+      expect(page).to have_content(@ingredient_1.name)
+      expect(page).to have_content(@ingredient_1.calories)
+      expect(page).to have_content(@ingredient_2.name)
+      expect(page).to have_content(@ingredient_2.calories)
+    end
+
+    it 'shows all the ingredients are unique' do
+      expect(page).to have_css("ingredient", :count => 2)
+    end
+
+    it 'shows all the ingredients are ordered by calories desc' do
+      expect(@ingredient_2.name).to appear_before(@ingredient_1.name)
     end
   end
 end
