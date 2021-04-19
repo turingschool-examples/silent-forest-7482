9c559ab0d8ee4d3413ab630774a57bbd6e2b7b55
George Ho <george.h.w.ho@gmail.com>
2021-04-19 11:55:25 -0400

    Add class method chef_ingredients

--- app/models/dish_ingredient.rb
+++ app/models/dish_ingredient.rb
@@ -1,4 +1,12 @@
 class DishIngredient < ApplicationRecord
   belongs_to :dish
   belongs_to :ingredient
+
+  def self.chef_ingredients(chef)
+    joins(:dish, :ingredient)
+    .select("ingredients.*")
+    .where("dishes.chef_id = ?", chef.id)
+    .order(calories: :desc)
+    .distinct
+  end
 end
