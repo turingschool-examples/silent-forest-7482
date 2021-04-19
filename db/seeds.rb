8f956c78d703a884d8a5ffa636c2a1bf3fb5c340
Aidan McGuire <>
2021-04-19 11:10:04 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+DishIngredient.destroy_all
+Ingredient.destroy_all
+Dish.destroy_all
+Chef.destroy_all
+
+chef = Chef.create!(name: "Anthony")
+dish1 = chef.dishes.create!(name: "Yum", description: "Very yum")
+dish2 = chef.dishes.create!(name: "Tasty", description: "Very tasty")
+ingredient2 = dish1.ingredients.create!(name: "carrot", calories: 20)
+ingredient1 = dish1.ingredients.create!(name: "onion", calories: 10)
+ingredient5 = dish2.ingredients.create!(name: "butter", calories: 50)
+ingredient4 = dish2.ingredients.create!(name: "garlic", calories: 40)
+ingredient3 = dish1.ingredients.create!(name: "potato", calories: 30)
